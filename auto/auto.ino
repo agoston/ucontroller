7755b3272ea555485c1bb8c89fca31c38716c519
Agoston Horvath <ahorvath@bol.com>
2016-01-23 00:54:50 +0100

    hooked up newping + hc-sr04, refactored code

--- auto/auto.ino
+++ auto/auto.ino
@@ -1,20 +1,30 @@
-// this means 3 KB (!!!)
+// this means 3-4 KB (!!!)
 //#define DEV
 
 /*
 Breadboard pin assignments:
 
-U24 = BA
-U23 = BB
+U24 = BA (L9110)
+U23 = BB (L9110)
 
-U22 = GND
-U21 = VCC
+U22 = GND (L9110)
+U21 = VCC (L9110)
 
-U20 = AA
-U19 = AB
+U20 = AA (L9110)
+U19 = AB (L9110)
+
+D21 = GND (HC-SR04)
+D20 = ECHO (HC-SR04)
+D19 = TRIG (HC-SR04)
+D18 = VCC (HC-SR04)
 
 U15 = VCC
 U14 = GND
+U13 = RST
+U12 = 5V
+
+D13 = RST
+D12 = GND
 
 D11 = D2
 D10 = D3 (PWM)
@@ -43,18 +53,75 @@ char buf[40];
 
 const byte PIN_MOTOR[4] = {6, 5, 9, 8}; // 2 pins per motor; A is PWM speed, B is direction
 #define PIN_LED 13     // use built-in LED
+#define PIN_TRIGGER 12
+#define PIN_ECHO 11
+#define MAX_DISTANCE_CM 100
+
+NewPing sonar(PIN_TRIGGER, PIN_ECHO, MAX_DISTANCE_CM);
+
 unsigned long lastTick;
 
+byte state_led = 0;
+
+byte state_motor[2] = {0,0};
+
+byte state_ping = 0;
+u16 distance = 0;
+
 /************************ L9110 */
-void drive(byte motor, int_fast16_t speed) {
+void drive(byte motor, u16 speed) {
   LOG("%d %d\n", motor, speed);
   digitalWrite(PIN_MOTOR[motor<<1], abs(speed));
   digitalWrite(PIN_MOTOR[(motor<<1)+1], speed < 0 ? LOW : HIGH);
 }
 
+void tick_motor() {
+  // TODO: slow start for motors - wheels lose grip instantly when going from zero to high in a single step
+  
+  // XXX
+  
+//  switch (state) {
+//    case 0:
+//      drive(0, 96); break;
+//    case 1:
+//      drive(1, 96); break;
+//    case 2:
+//      drive(0, 0); break;
+//    case 3:
+//      drive(1, 0); break;
+//  }
+}
+
+/************************ LED */
+void tick_led() {
+  // blink about once per second
+  byte new_led = (lastTick >> 10) & 1;
+  if (new_led != state_led) {
+    digitalWrite(LED_BUILTIN, (new_led > 0 ? HIGH : LOW));
+    state_led = new_led;
+  }
+}
+
+/************************ HC-SR04 */
+void tick_ping() {
+  // measure about 10 times per second
+  byte new_ping = (lastTick >> 7) & 15;
+  if (new_ping != state_ping) {
+    unsigned int uS = sonar.ping();
+    
+    if (uS > US_ROUNDTRIP_CM) {     // ignore measurement errors
+      distance = uS / US_ROUNDTRIP_CM;
+      LOG("Measured %d cm", distance);
+    }
+    
+    state_ping = new_ping;
+  }
+}
+
 /************************ atmega168/328 */
 void setup() {
 #ifdef DEV
+  delay(50);  // bootloader listens for firmware update, should not get garbage, wait a bit
   Serial.begin(9600);
 #endif
 
@@ -77,29 +144,13 @@ void loop() {
   }
 
   if (input == 't') {
-    LOG("Serial test... %c", input);
+    LOG("Serial test...", 0);
   }
 #endif
 
-  // blink about once per second
   lastTick = millis();
-  byte state = (lastTick >> 10) & 3;
   
-  byte led = state & 1;
-  LOG("lastTick: %ul, led: %d\n", lastTick, led);
-  digitalWrite(LED_BUILTIN, (led > 0 ? HIGH : LOW));
-
-  // TODO: slow start for motors - wheels lose grip instantly when going from zero to high in a single step
-  // TODO: ping
-
-  switch (state) {
-    case 0:
-      drive(0, 96); break;
-    case 1:
-      drive(1, 96); break;
-    case 2:
-      drive(0, 0); break;
-    case 3:
-      drive(1, 0); break;
-  }
+  tick_led();
+  tick_ping();
+  tick_motor();
 }
