eb711f1f9c554a5e90af60f9c2350873c1e7cf1c
Agoston Horvath <ahorvath@bol.com>
2016-07-01 00:03:13 +0200

    joystick works (... with 3 analog ins - meh), rest is heavily WIP

--- auto_rfm69_sender/auto_rfm69_sender.ino
+++ auto_rfm69_sender/auto_rfm69_sender.ino
@@ -1,6 +1,6 @@
-//#define DEV
+#define DEV
 
-#include <LowPower.h>
+//#include <LowPower.h>
 #include <SPI.h>
 #include <RFM69.h>
 #include <agoston.h>
@@ -25,12 +25,14 @@
  D01 = D12
  */
 
-// this one gotta be 128 bits long
-#define ENCRYPTKEY      "1;59IUa\"sNL2EA2M"
+#define PIN_JOYX 0
+#define PIN_JOYY 1
+#define PIN_JOYSW 2
 
 RFM69 radio;
 
-uint8_t nodeId = 241;
+uint8_t senderId = 1;
+uint8_t carId = 10;
 uint8_t networkId = 117;
 unsigned long lastTick;
 
@@ -41,10 +43,18 @@ void setup() {
 	Serial.begin(115200);
 #endif
 
-	radio.initialize(RF69_433MHZ, nodeId, networkId);
-	// don't need that kind of power just yet...
-	//radio.setHighPower();
-	radio.encrypt(ENCRYPTKEY);
+	// FIXME: Hard Reset the RFM module
+//	pinMode(RFM69_RST, OUTPUT);
+//	digitalWrite(RFM69_RST, HIGH);
+//	delay(100);
+//	digitalWrite(RFM69_RST, LOW);
+//	delay(100);
+
+	// rfm69
+	radio.initialize(RF69_433MHZ, senderId, networkId);
+	radio.setHighPower();	// NB, this is not optional, despite the name
+	radio.setPowerLevel(5);	// FIXME: set power lever enough for 10-20 meters with clear LoS
+	radio.encrypt(null);
 	lastTick = millis();
 }
 
@@ -63,9 +73,37 @@ void loop() {
 	}
 #endif
 
-//  radio.sendWithRetry(GATEWAYID, "RING", 4);
+    int x = analogRead(PIN_JOYX);
+    int y = analogRead(PIN_JOYY);
+    int sw = analogRead(PIN_JOYSW);
+    LOG("%d %d %d", x, y, sw);
+    delay(250);
 
-	if (radio.receiveDone()) {
+
+	// read joystick state
+
+	// compare joystick state
+
+	// send if changed
+	//radio.send(carId, buf, buflen);
+
+	// FIXME: after X hundred ms, send it anyway -- to make sure car won't power down, thinking remote went idle
+	// FIXME: after no change in joystick for 30s, start powering down for 15s and no radio (power-save mode)
+
+	// power down
+//	LowPower.powerDown(SLEEP_15MS, ADC_OFF, BOD_OFF);
+	// FIXME: also power down rfm69
+}
+
+/************************ TICK */
+uint_fast16_t tick() {
+	unsigned long newTick = millis();
+	uint_fast16_t elapsed = newTick - lastTick;
+	lastTick = newTick;
+	return elapsed;
+}
+
+//if (radio.receiveDone()) {
 //		LOG(radio.SENDERID);
 //		for (byte i = 0; i < radio.DATALEN; i++)
 //			LOG((char) radio.DATA[i]);
@@ -73,7 +111,7 @@ void loop() {
 //		if (radio.DATALEN == 3) {
 //
 //		}
-	}
+//}
 
 // TODO: ACK
 //	if (radio.ACKRequested()) {
@@ -94,14 +132,3 @@ void loop() {
 //			STATUS == STATUS_OPEN ? "OPEN" : "UNKNOWN");
 //	byte len = strlen(buff);
 //	radio.sendWithRetry(GATEWAYID, buff, len);
-}
-
-/************************ TICK */
-uint_fast16_t tick() {
-	LowPower.powerDown(SLEEP_30MS, ADC_OFF, BOD_OFF);
-	unsigned long newTick = millis();
-	uint_fast16_t elapsed = newTick - lastTick;
-	lastTick = newTick;
-	return elapsed;
-}
-
