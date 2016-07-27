e712d7e966fa3952dbc396f1e1e234fc3cf24d8d
Agoston Horvath <ahorvath@bol.com>
2016-07-27 22:24:50 +0200

    added fish feeder

---
+++ fish_feeder/fish_feeder.ino
@@ -0,0 +1,60 @@
+//#define DEV
+#include <agoston.h>
+//#include <LowPower.h>
+
+// pin for optocoupler
+#define PIN_OC  3
+
+const unsigned long STATE_LENGTH[2] = {8*3600ul*1000ul, 16*3600ul*1000ul};
+unsigned long state_next_at = 0;
+byte state = 0;
+
+void setup() {
+#ifdef DEV
+	delay(50); // bootloader listens for firmware update, should not get garbage, wait a bit
+	Serial.begin(115200);
+#endif
+
+  pinAsOutput(LED_BUILTIN);
+  digitalLow(LED_BUILTIN);
+  pinAsOutput(PIN_OC);
+  digitalLow(PIN_OC);
+}
+
+void loop() {
+#ifdef DEV
+	if (Serial.available()) {
+		input = Serial.read();
+	} else {
+		input = 0;
+	}
+
+	if (input == 't') {
+		LOG("Serial test...", 0);
+	}
+
+  if (input == '1') {
+    LOG("1", NULL);
+    digitalHigh(LED_BUILTIN);
+    digitalHigh(PIN_OC);
+  }
+
+  if (input == '2') {
+    LOG("2", NULL);
+    digitalLow(LED_BUILTIN);
+    digitalLow(PIN_OC);
+  }
+#endif
+  
+  unsigned long now = millis();
+  if (now > state_next_at) {
+      state = !state;
+      state_next_at += STATE_LENGTH[state];
+
+      digitalWrite(LED_BUILTIN, state);
+      digitalWrite(PIN_OC, state);
+  }
+
+//	LowPower.powerDown(SLEEP_30MS, ADC_OFF, BOD_OFF);
+	delay(20);	// wakey after deep sleep
+}
