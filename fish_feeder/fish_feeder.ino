56c6539be6813aaa6ec30b20a343855aabbe4e75
Agoston Horvath <ahorvath@bol.com>
2016-07-27 22:57:32 +0200

    switched the default (unpowered) state to 'ON' -- so that if power goes away, battery will stay connected to the feeder

--- fish_feeder/fish_feeder.ino
+++ fish_feeder/fish_feeder.ino
@@ -5,14 +5,14 @@
 // pin for optocoupler
 #define PIN_OC  3
 
-const unsigned long STATE_LENGTH[2] = {8*3600ul*1000ul, 16*3600ul*1000ul};
+const unsigned long STATE_LENGTH[2] = {16 * 3600ul * 1000ul, 8 * 3600ul * 1000ul};
 unsigned long state_next_at = 0;
 byte state = 0;
 
 void setup() {
 #ifdef DEV
-	delay(50); // bootloader listens for firmware update, should not get garbage, wait a bit
-	Serial.begin(115200);
+  delay(50); // bootloader listens for firmware update, should not get garbage, wait a bit
+  Serial.begin(115200);
 #endif
 
   pinAsOutput(LED_BUILTIN);
@@ -23,15 +23,15 @@ void setup() {
 
 void loop() {
 #ifdef DEV
-	if (Serial.available()) {
-		input = Serial.read();
-	} else {
-		input = 0;
-	}
+  if (Serial.available()) {
+    input = Serial.read();
+  } else {
+    input = 0;
+  }
 
-	if (input == 't') {
-		LOG("Serial test...", 0);
-	}
+  if (input == 't') {
+    LOG("Serial test...", 0);
+  }
 
   if (input == '1') {
     LOG("1", NULL);
@@ -45,16 +45,17 @@ void loop() {
     digitalLow(PIN_OC);
   }
 #endif
-  
+
   unsigned long now = millis();
   if (now > state_next_at) {
-      state = !state;
-      state_next_at += STATE_LENGTH[state];
+    state = !state;
+    state_next_at += STATE_LENGTH[state];
 
-      digitalWrite(LED_BUILTIN, state);
-      digitalWrite(PIN_OC, state);
+    digitalWrite(LED_BUILTIN, state);
+    digitalWrite(PIN_OC, state);
   }
 
-//	LowPower.powerDown(SLEEP_30MS, ADC_OFF, BOD_OFF);
-	delay(20);	// wakey after deep sleep
+  // not available on atmega168
+  //	LowPower.powerDown(SLEEP_30MS, ADC_OFF, BOD_OFF);
+  delay(20);	// wakey after deep sleep
 }
