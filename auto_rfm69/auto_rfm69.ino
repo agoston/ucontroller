d94a696fb78a72e2370662b07fee00dd2455ff05
Agoston Horvath <ahorvath@bol.com>
2016-09-25 17:42:00 +0200

    data transfer over rfm69 works!!! woohoo! (for the first time)

--- auto_rfm69/auto_rfm69.ino
+++ auto_rfm69/auto_rfm69.ino
@@ -9,128 +9,117 @@
 // TODO: wireless programming support (-> remember to hide encrypt keys!)
 // TODO: use RFM69_ATC to conserve battery
 
-/*
- Breadboard pin assignments:
+#define NSS_PIN 10
+#define IRQ_PIN 3
 
- D11 = D2
- D10 = D3 (PWM)
- D09 = D4
- D08 = D5 (PWM)
- D07 = D6 (PWM)
- D06 = D7
- D05 = D8
- D04 = D9 (PWM)
- D03 = D10 (PWM)
- D02 = D11 (PWM)
- D01 = D12
- */
-
-#define PIN_JOYX 0
-#define PIN_JOYY 1
-#define PIN_JOYSW 2
-
-RFM69 radio;
+RFM69 radio(NSS_PIN, IRQ_PIN, true, digitalPinToInterrupt(IRQ_PIN));
 
 uint8_t senderId = 1;
 uint8_t carId = 10;
 uint8_t networkId = 117;
 unsigned long lastTick;
 
-
+uint8_t rf69buf[RF69_MAX_DATA_LEN];
 
 /************************ INIT */
 void setup() {
 #ifdef DEV
-	delay(50); // bootloader listens for firmware update, should not get garbage, wait a bit
-	Serial.begin(115200);
+  delay(50); // bootloader listens for firmware update, should not get garbage, wait a bit
+  Serial.begin(115200);
 #endif
 
-	// FIXME: Hard Reset the RFM module
-//	pinMode(RFM69_RST, OUTPUT);
-//	digitalWrite(RFM69_RST, HIGH);
-//	delay(100);
-//	digitalWrite(RFM69_RST, LOW);
-//	delay(100);
+  // FIXME: Hard Reset the RFM module
+  //  pinMode(RFM69_RST, OUTPUT);
+  //  digitalWrite(RFM69_RST, HIGH);
+  //  delay(100);
+  //  digitalWrite(RFM69_RST, LOW);
+  //  delay(100);
 
-	// rfm69
-	radio.initialize(RF69_433MHZ, senderId, networkId);
-	radio.setHighPower();	// NB, this is not optional, despite the name
-	radio.setPowerLevel(5);	// FIXME: set power lever enough for 10-20 meters with clear LoS
-	radio.encrypt(null);
-	lastTick = millis();
+  // rfm69
+  radio.initialize(RF69_433MHZ, carId, networkId);
+  //  radio.setHighPower(); // NB, this is not optional, despite the name
+  radio.setPowerLevel(5); // FIXME: set power lever enough for 10-20 meters with clear LoS
+  //  radio.encrypt(null);
+  lastTick = millis();
 }
 
 /************************ LOOP */
 
 void loop() {
 #ifdef DEV
-	if (Serial.available()) {
-		input = Serial.read();
-	} else {
-		input = 0;
-	}
+  if (Serial.available()) {
+    input = Serial.read();
+  } else {
+    input = 0;
+  }
 
-	if (input == 't') {
-		LOG("Serial test...", 0);
-	}
+  if (input == 't') {
+    LOG("Serial test... %d", LED_BUILTIN);
+  }
 #endif
 
-    int x = analogRead(PIN_JOYX);
-    int y = analogRead(PIN_JOYY);
-    int sw = analogRead(PIN_JOYSW);
-    LOG("%d %d %d", x, y, sw);
-    delay(250);
+  if (radio.receiveDone()) {
+    LOG("Sender: %u, Datalen: %u", radio.SENDERID, radio.DATALEN);
+    digitalHigh(LED_BUILTIN);
+  }
 
+  delay(50);
+  digitalLow(LED_BUILTIN);
 
-	// read joystick state
+  // read joystick state
+  //  int x = analogRead(PIN_JOYX);
+  //  int y = analogRead(PIN_JOYY);
+  //  int sw = analogRead(PIN_JOYSW);
+  //  LOG("%d %d %d", x, y, sw);
+  //  delay(250);
+  //
 
-	// compare joystick state
+  // compare joystick state
 
-	// send if changed
-	radio.send(carId, buf, buflen);
+  // send if changed
 
-	// FIXME: after X hundred ms, send it anyway -- to make sure car won't power down, thinking remote went idle
-	// FIXME: after no change in joystick for 30s, start powering down for 15s and no radio (power-save mode)
+  // FIXME: after X hundred ms, send it anyway -- to make sure car won't power down, thinking remote went idle
+  // FIXME: after no change in joystick for 30s, start powering down for 15s and no radio (power-save mode)
 
-	// power down
-//	LowPower.powerDown(SLEEP_15MS, ADC_OFF, BOD_OFF);
-	// FIXME: also power down rfm69
+  // power down
+  //  LowPower.powerDown(SLEEP_15MS, ADC_OFF, BOD_OFF);
+  // FIXME: also power down rfm69
 }
 
 /************************ TICK */
 uint_fast16_t tick() {
-	unsigned long newTick = millis();
-	uint_fast16_t elapsed = newTick - lastTick;
-	lastTick = newTick;
-	return elapsed;
+  unsigned long newTick = millis();
+  uint_fast16_t elapsed = newTick - lastTick;
+  lastTick = newTick;
+  return elapsed;
 }
 
 //if (radio.receiveDone()) {
-//		LOG(radio.SENDERID);
-//		for (byte i = 0; i < radio.DATALEN; i++)
-//			LOG((char) radio.DATA[i]);
+//    LOG(radio.SENDERID);
+//    for (byte i = 0; i < radio.DATALEN; i++)
+//      LOG((char) radio.DATA[i]);
 //
-//		if (radio.DATALEN == 3) {
+//    if (radio.DATALEN == 3) {
 //
-//		}
+//    }
 //}
 
 // TODO: ACK
-//	if (radio.ACKRequested()) {
-//		radio.sendACK();
-//		LOG(" - ACK sent.");
-//	}
+//  if (radio.ACKRequested()) {
+//    radio.sendACK();
+//    LOG(" - ACK sent.");
+//  }
 
 // TODO: send
-//	sprintf(sendBuf, "F:%d H:%d P:%s", weatherShield_SI7021.getFahrenheitHundredths(), weatherShield_SI7021.getHumidityPercent(), Pstr);
-//	byte sendLen = strlen(sendBuf);
-//	radio.send(GATEWAYID, sendBuf, sendLen);
+//  sprintf(sendBuf, "F:%d H:%d P:%s", weatherShield_SI7021.getFahrenheitHundredths(), weatherShield_SI7021.getHumidityPercent(), Pstr);
+//  byte sendLen = strlen(sendBuf);
+//  radio.send(GATEWAYID, sendBuf, sendLen);
 
-//	char buff[10];
-//	sprintf(buff,
-//			STATUS == STATUS_CLOSED ? "CLOSED" :
-//			STATUS == STATUS_CLOSING ? "CLOSING" :
-//			STATUS == STATUS_OPENING ? "OPENING" :
-//			STATUS == STATUS_OPEN ? "OPEN" : "UNKNOWN");
-//	byte len = strlen(buff);
-//	radio.sendWithRetry(GATEWAYID, buff, len);
+//  char buff[10];
+//  sprintf(buff,
+//      STATUS == STATUS_CLOSED ? "CLOSED" :
+//      STATUS == STATUS_CLOSING ? "CLOSING" :
+//      STATUS == STATUS_OPENING ? "OPENING" :
+//      STATUS == STATUS_OPEN ? "OPEN" : "UNKNOWN");
+//  byte len = strlen(buff);
+//  radio.sendWithRetry(GATEWAYID, buff, len);
