8da370811ab8380f3e8355f5232697f988202488
Agoston Horvath <ahorvath@bol.com>
2017-06-23 13:45:05 +0200

    massive refactor, platformio workflow more or less works

--- mesh_rfm69/src/mesh.cpp
+++ mesh_rfm69/src/mesh.cpp
@@ -1,3 +1,14 @@
+#define DEV
+
+// #define SERVER
+// #define m_self m_seagoat
+
+#define CLIENT
+#define m_self m_achterlamp
+#define ACHTERLAMP
+
+#include <Arduino.h>
+
 //#include <LowPower.h>
 #include <SPI.h>
 #include <RFM69.h>
@@ -9,73 +20,82 @@
 
 #define NSS_PIN 10
 #define IRQ_PIN 3
+#define RFM69_RST 8
 
 RFM69 radio(NSS_PIN, IRQ_PIN, true, digitalPinToInterrupt(IRQ_PIN));
 
-#define m_self m_seagoat
 const uint8_t networkId = 117;
 const uint8_t m_seagoat = 1;
 const uint8_t m_achterlamp = 2;
 
+typedef struct __attribute__((packed)) {
+	uint8_t type;
+	union {
+		uint8_t relay1;
+	} payload;
+} Message;
+
 uint8_t serialbuf[16];
 uint8_t serialidx = 0;
 
-typedef struct {
-	byte relayOn = 0;
-} Message;
-
 /************************ INIT */
 void setup() {
+#if defined(DEV) || defined(SERVER)
 	delay(50); // bootloader listens for firmware update, should not get garbage, wait a bit
 	Serial.begin(115200);
+#endif
+
+	pinMode(LED_BUILTIN, OUTPUT);
 
 	// FIXME: Hard Reset the RFM module -- seemingly unnecessary
-	//	pinMode(RFM69_RST, OUTPUT);
-	//	digitalWrite(RFM69_RST, HIGH);
-	//	delay(100);
-	//	digitalWrite(RFM69_RST, LOW);
-	//	delay(100);
+	// pinMode(RFM69_RST, OUTPUT);
+	// digitalWrite(RFM69_RST, HIGH);
+	// delay(100);
+	// digitalWrite(RFM69_RST, LOW);
+	// delay(100);
 
 	// rfm69
 	radio.initialize(RF69_433MHZ, m_self, networkId);
   radio.setHighPower();
-	// radio.setPowerLevel(5);
+	radio.setPowerLevel(5);
   radio.encrypt(null);
 }
 
-/************************ LOOP */
-uint8_t remoteLed = 0;
-
 void mesh_send(uint8_t to, void *data, uint8_t len) {
-	uint8_t buf[len+1];
-	buf[0] = 0;
-	memcpy(&(buf[1]), data, len);
-	radio.send(to, buf, len + 1);
+	// TODO: sendWithRetry/sendACK?
+	radio.send(to, data, len);
 }
 
+//############################################################################
+#ifdef SERVER
+uint8_t remoteLed = 0;
+
 void toggle() {
-	remoteLed = !remoteLed;
+ 	remoteLed = remoteLed == HIGH ? LOW : HIGH;
 
 	Message message;
-	message.relayOn = remoteLed;
+	message.type = 0;
+	message.payload.relay1 = remoteLed;
 
 	mesh_send(m_achterlamp, &message, sizeof(message));
 }
 
 void runCommand(uint8_t *buf, uint8_t len) {
-	Serial.println((char *) buf);
+	LOG((char *) buf);
 	if (!memcmp(buf, "t", len)) {
 		toggle();
 	} else {
-		Serial.println('E');
+		LOG('E');
 		return;
 	}
-	Serial.println('.');
+	LOG('.');
 }
 
-
-
 void loop() {
+	delay(1000);
+	toggle();
+	if (1==1) return;
+
 	while (Serial.available()) {
 		uint8_t input = Serial.read();
 		if (input == ';') {
@@ -87,12 +107,30 @@ void loop() {
 			serialidx = (serialidx + 1) & 0xf;
 		}
 	}
-	//
-	// digitalHigh(LED_BUILTIN);
-	// delay(200);
-	// digitalLow(LED_BUILTIN);
-	// delay(300);
 }
+#endif
+
+//############################################################################
+#ifdef ACHTERLAMP
+void mesh_receive(uint8_t sender, Message *in, uint8_t len) {
+	if (in->type == 0) {
+		LOG("relay1: %d", in->payload.relay1);
+		digitalWrite(LED_BUILTIN, in->payload.relay1);
+	} else {
+		LOG("incorrect data type %d", in->type);
+	}
+}
+
+void loop() {
+	if (radio.receiveDone()) {
+    LOG("Sender: %u, Datalen: %u", radio.SENDERID, radio.DATALEN);
+		mesh_receive(radio.SENDERID, (Message *)(&radio.DATA), radio.DATALEN);
+  }
+
+  delay(50);
+}
+#endif
+
 
 // FIXME: after X hundred ms, send it anyway -- to make sure car won't power down, thinking remote went idle
 // FIXME: after no change in joystick for 30s, start powering down for 15s and no radio (power-save mode)
