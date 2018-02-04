2148a7fbd42bab753726e5e84aa8e47d19cefc88
Agoston Horvath <ahorvath@bol.com>
2018-02-04 01:07:45 +0100

    made animpixel super efficient

--- esp8285_led_string/src/main.cpp
+++ esp8285_led_string/src/main.cpp
@@ -9,6 +9,10 @@ const uint8_t ROWS = 8;
 const uint8_t COLUMNS = 8;
 const uint8_t LEDS = 60;
 
+const uint8_t ANIM_MS = 125;
+const uint8_t ANIM_TICK = 8;
+const uint8_t ANIM_TICK_SHIFT = 3;
+
 // uses GPIO2 (hardwired)
 NeoPixelBus<NeoGrbFeature, NeoEsp8266AsyncUart800KbpsMethod> strip(LEDS);
 
@@ -26,8 +30,11 @@ const char *img = "XyyyyyyX"
 
 char *reverse(char *s, uint16_t len) {
   char *e = s + len;
+  char temp;
   while (s < e) {
-    std::swap(*s, *e);
+    temp = *s;
+    *s = *e;
+    *e = temp;
     s++;
     e--;
   }
@@ -45,26 +52,54 @@ void translatePhysicalLayout(uint16_t columns, uint16_t rows, const char *orig_i
 
 //----------------------------------------------------------------------------------------------------------------
 class AnimPixel {
-  uint8_t r, g, b;
-  int8_t dr, dg, db;
-  uint8_t maxDelta;
+  uint16_t r, g, b;
+  int16_t dr, dg, db;
+  uint8_t minr, ming, minb;
+  uint8_t randr, randg, randb;
+  uint8_t tick;
 
 public:
   void step() {
-    // TODO: add random so that it glows nicely
+    if (tick >= ANIM_TICK) {
+      uint8_t newr = minr + random(randr);
+      uint8_t newg = ming + random(randg);
+      uint8_t newb = minb + random(randb);
+
+      dr = (newr - r) << (8-ANIM_TICK_SHIFT);
+      dg = (newg - g) << (8-ANIM_TICK_SHIFT);
+      db = (newb - b) << (8-ANIM_TICK_SHIFT);
+      tick = 0;
+    } else {
+      tick++;
+    }
+
+    r += dr;
+    g += dg;
+    b += db;
   }
 
-  void init(uint8_t pr, uint8_t pg, uint8_t pb, uint8_t pmaxDelta) {
+  void init(uint8_t pr, uint8_t pg, uint8_t pb, uint8_t maxDelta) {
     r = pr;
     g = pg;
     b = pb;
-    maxDelta = pmaxDelta;
+
+    uint8_t minr = max(0, r - maxDelta);
+    uint8_t ming = max(0, g - maxDelta);
+    uint8_t minb = max(0, b - maxDelta);
+    uint8_t maxr = min(255, r + maxDelta);
+    uint8_t maxg = min(255, g + maxDelta);
+    uint8_t maxb = min(255, b + maxDelta);
+    randr = maxr - minr;
+    randg = maxg - ming;
+    randb = maxb - minb;
+
+    tick = 255;
   }
 
   void writeGrb(uint8_t *p) {
-    *(p++) = g;
-    *(p++) = r;
-    *(p++) = b;
+    *(p++) = g >> 8;
+    *(p++) = r >> 8;
+    *(p++) = b >> 8;
   }
 };
 
