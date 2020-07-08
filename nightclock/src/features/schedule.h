05d7f405f14d69b62cfa830e7cb50cd829675abf
Agoston Horvath <ahorvath@bol.com>
2020-07-08 23:07:15 +0200

    WIP

--- nightclock/src/features/schedule.h
+++ nightclock/src/features/schedule.h
@@ -2,37 +2,65 @@
 #define __FEATURES_SCHEDULE_H
 
 #include <Arduino.h>
+
 #include "feature.h"
-#include "ntpclient.h"
 #include "log.h"
+#include "ntpclient.h"
+
+class ScheduledTime {
+   public:
+    ScheduledTime *next = NULL;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hours;
+    uint8_t minutes;
+    void (*callback)(void *);
+    void *arg;
+
+    ScheduledTime(uint8_t month, uint8_t day, uint8_t hours, uint8_t minutes, void (*callback)(void *), void *arg) : month(month), day(day), hours(hours), minutes(minutes), callback(callback), arg(arg){};
+
+    bool operator<(const ScheduledTime &foo) const { return month < foo.month || day < foo.day || hours < foo.hours || minutes < foo.minutes; }
+};
 
 class Schedule : public Feature {
-    private:
+   private:
     NtpClient *ntpClient;
     ScheduledTime *root = NULL;
 
-    public:
-    Schedule(NtpClient *ntpClient) : ntpClient(ntpClient) {};
-    ~Schedule() {
-        for (ScheduledTime *t = root; t != NULL; ) {
+   public:
+    Schedule(NtpClient *ntpClient) : ntpClient(ntpClient){};
+    Schedule(const Schedule *const other) {operator=(other);}
+    virtual ~Schedule() { cleanup(); }
+
+    void cleanup() {
+        for (ScheduledTime *t = root; t != NULL;) {
             ScheduledTime *n = t->next;
             delete t;
             t = n;
         }
+        root = NULL;
+    }
+
+    Schedule *operator=(const Schedule *const other) {
+        if (this == other) return this;
+
+        cleanup();
+        root = other->root;
+        return this;
     }
 
     void setup() {
     }
 
-    void schedule(uint8_t month, uint8_t day, uint8_t hours, uint8_t mins, void (*callback)(void*), void *arg) {
+    void schedule(uint8_t month, uint8_t day, uint8_t hours, uint8_t mins, void (*callback)(void *), void *arg) {
         ScheduledTime *scheduledTime = new ScheduledTime(month, day, hours, mins, callback, arg);
-        
+
         // pfff this is some ugly ass ad-hoc sorted linked list
         ScheduledTime *next = root, *act = NULL;
         do {
-            if (!next) {   // insert to end
+            if (!next) {  // insert to end
                 if (!act) {
-                    root = scheduledTime; 
+                    root = scheduledTime;
                 } else {
                     act->next = scheduledTime;
                 }
@@ -40,7 +68,7 @@ class Schedule : public Feature {
             }
 
             if (*next < *scheduledTime) {
-                act = next; 
+                act = next;
                 next = next->next;
                 continue;
             }
@@ -57,35 +85,29 @@ class Schedule : public Feature {
     }
 
     void loop() {
+        if (!root) return;
+
         uint8_t month = ntpClient->month();
         uint8_t day = ntpClient->day();
         uint8_t hours = ntpClient->hour();
         uint8_t minutes = ntpClient->minute();
 
-        // root is smallest
-        if (month < root->month || day < root->day || hours < root->hours || minutes < root->minutes) return;
+        // find today (leaving past entries in, so that even if schedule server falls out, this keeps working for up to a year)
+        for (ScheduledTime *s = root, **pNext = &root; s; pNext = &(s->next), s = s->next) {
+            if (month != s->month || day != s->day) continue;
 
-        root->callback(root->arg);
+            // found today; is it time?
+            if (hours < root->hours && minutes < root->minutes) return;
 
-        ScheduledTime *t = root;
-        root = root->next;
-        delete t;
+            // match! run
+            s->callback(s->arg);
+
+            // delete from linked list, so it won't run again
+            *pNext = s->next;
+            delete s;
+            return;
+        }
     }
-};
-
-class ScheduledTime {
-public:
-    ScheduledTime *next = NULL;
-    uint8_t month;
-    uint8_t day;
-    uint8_t hours;
-    uint8_t minutes;
-    void (*callback)(void*);
-    void *arg;
-
-    ScheduledTime(uint8_t month, uint8_t day, uint8_t hours, uint8_t minnutes, void (*callback)(void*), void *arg) : month(month), day(day), hours(hours), minutes(minutes), callback(callback), arg(arg) {};
-
-    bool operator<(const ScheduledTime &foo) const { return month < foo.month || day < foo.day || hours < foo.hours || minutes < foo.minutes; }
 };
 
 #endif
--- nightclock/src/schuurlamp.cpp
+++ nightclock/src/schuurlamp.cpp
@@ -1,106 +1,116 @@
 #define DEV
 
 #include <Arduino.h>
-
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-
 #include <features/log.h>
-#include <features/mesh.h>
 #include <features/ntpclient.h>
 #include <features/relay.h>
+#include <features/schedule.h>
 
 #include "secret.h"
 
 // relay
-Relay relay(D1);
+Relay relay(D0);
 // sync time from NTP
 NtpClient ntpClient;
-// send/receive UDP packets
-Mesh mesh(HEAD_PORT);
+// scheduling
+Schedule schedule(&ntpClient);
 
-Feature *features[]{&relay, &ntpClient, &mesh};
+Feature *features[]{&relay, &ntpClient, &schedule};
 
 WiFiClient tcpClient;
 HTTPClient httpClient;
 
+uint8_t lastFetchMonth = 0;
+uint8_t lastFetchDay = 0;
 
 //----------------------------------------------------------------------------------------------------------------
 void setup() {
 #ifdef DEV
-  delay(50);
-  Serial.begin(9600);
+    delay(50);
+    Serial.begin(9600);
 #endif
 
-  // since there is a single radio in esp8266, when connecting to an remote AP,
-  // it will change the wifi channel for the clients of this AP too. clients of
-  // this AP can follow that, but e.g. tcp connections might get interrupted.
-  WiFi.mode(WIFI_STA);
+    // upstream internet access for NTP sync
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(NTP_SSID, NTP_PW);
+    LOG("Waiting for wireless")
 
-  // upstream internet access for NTP sync
-  WiFi.begin(NTP_SSID, NTP_PW);
-
-  LOGP("VCC: %d", ESP.getVcc())
-
-  for (uint16_t i = 0; i < sizeof(features) / sizeof(features[0]); i++) features[i]->setup();
-}
-
-void fetchSchedule(uint8_t month, uint8_t day, uint8_t hours, uint8_t mins) {
-  if (httpClient.begin(tcpClient, BASEURL "/schuurlamp/schedule")) {
-    // start connection and send HTTP header
-    int httpCode = httpClient.GET();
-
-    // httpCode will be negative on error
-    if (httpCode >= 200 && httpCode < 400) {
-      while (httpClient.connected()) {
-        // format: 0110 08:44 - 16:51
-        String line = tcpClient.readStringUntil('\n');
-        if (line.isEmpty()) break;
-        if (line.length() != 18) continue;
-
-        String date = line.substring(0, 4);
-        if (date == "****" || (date.substring(0, 2).toInt() == month && date.substring(2, 4).toInt() == day)) {
-          // this line applies today, schedule
-          uint8_t beginHour = line.substring(5,7).toInt();
-          uint8_t beginMin = line.substring(8,10).toInt();
-          uint8_t endHour = line.substring(13,15).toInt();
-          uint8_t endMin = line.substring(16,18).toInt();
-          
-          os_timer_setfn(&onTimer, &trampolineOn, &relay);
-          os_timer_arm(&onTimer, 1000, false);
-
-          os_timer_setfn(&offTimer, &trampolineOn, &relay);
-          os_timer_arm(&offTimer, 1000, false);
-
-
-        }
-      }
-    } else {
-      Serial.printf("[HTTP] GET... failed, error: %s\n", httpClient.errorToString(httpCode).c_str());
+    // dhcp starts now in background (unless static IP)
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(50);
     }
+    LOGP("Got IP: %s", WiFi.localIP().toString().c_str())
 
-    httpClient.end();
-  }
+    // enter into light sleep between DTIM updates, ~1mA consumption
+    WiFi.setSleepMode(WIFI_LIGHT_SLEEP);
+
+    for (uint16_t i = 0; i < sizeof(features) / sizeof(features[0]); i++) features[i]->setup();
 }
 
 void trampolineOn(void *relay) {
-  ((Relay*)relay)->on();
+    ((Relay *)relay)->on();
 }
 
 void trampolineOff(void *relay) {
-  ((Relay*)relay)->off();
+    ((Relay *)relay)->off();
+}
+
+Schedule *fetchSchedule() {
+    Schedule *ret = NULL;
+    if (httpClient.begin(tcpClient, BASEURL "/schuurlamp/schedule")) {
+        // start connection and send HTTP header
+        int httpCode = httpClient.GET();
+
+        // httpCode will be negative on error
+        if (httpCode >= 200 && httpCode < 400) {
+            ret = new Schedule(&ntpClient);
+
+            while (httpClient.connected()) {
+                // format: 0110 08:44 - 16:51
+                String line = tcpClient.readStringUntil('\n');
+                if (line.isEmpty()) break;
+                if (line.length() != 18) continue;
+
+                String date = line.substring(0, 4);
+                uint8_t month = date.substring(0, 2).toInt();
+                uint8_t day = date.substring(2, 4).toInt();
+                uint8_t beginHour = line.substring(5, 7).toInt();
+                uint8_t beginMin = line.substring(8, 10).toInt();
+                uint8_t endHour = line.substring(13, 15).toInt();
+                uint8_t endMin = line.substring(16, 18).toInt();
+
+                ret->schedule(month, day, beginHour, beginMin, &trampolineOn, &relay);
+                ret->schedule(month, day, endHour, endMin, &trampolineOff, &relay);
+            }
+        } else {
+            LOGP("HTTP status code %d", httpCode);
+        }
+
+        httpClient.end();
+    }
+
+    return ret;
 }
 
 //----------------------------------------------------------------------------------------------------------------
 void loop() {
-  for (uint16_t i = 0; i < sizeof(features) / sizeof(features[0]); i++) features[i]->loop();
+    for (uint16_t i = 0; i < sizeof(features) / sizeof(features[0]); i++) features[i]->loop();
 
+    LOGP("TIME: %d:%d:%d", ntpClient.hour(), ntpClient.minute(), ntpClient.second())
 
-  // can't use deep sleep here, as it would turn off the modem, which defeats
-  // the purpose of a udp server. however, the esp-arduino lib does an actual
-  // yield() on delay(), so consumption is kept to a minimum.
-  delay(250);
-  
-  // ESP.deepSleep() wow
+    // re-fetch schedule once a day
+    if (lastFetchMonth != ntpClient.month() || lastFetchDay != ntpClient.day()) {
+        // fetchSchedule();
+        // if (Schedule *fresh = fetchSchedule()) {
+        //     schedule = fresh;
+            // lastFetchMonth = ntpClient.month();
+            // lastFetchDay = ntpClient.day();
+        // }
+    }
+
+    delay(1500);
+    // delay(15000);
 }
